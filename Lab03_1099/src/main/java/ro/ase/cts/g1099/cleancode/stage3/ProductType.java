eb1c39d52d4e967f88ca528828e0fcf29aab86f3
roberttj <robert21tj@gmail.com>
2022-03-10 22:45:34 +0200

    Third lab

--- Lab03_1099/src/main/java/ro/ase/cts/g1099/cleancode/stage3/ProductType.java
+++ Lab03_1099/src/main/java/ro/ase/cts/g1099/cleancode/stage3/ProductType.java
@@ -1,7 +1,7 @@
 package ro.ase.cts.g1099.cleancode.stage3;
 
 public enum ProductType {
-    NEW(1, 0), DISCOUNT(2, 0.15f), SALES(3, 0.25f), FINAL_SALES(3, 0.35f);
+    NEW(1, 0), DISCOUNT(2, 0.15f), SALES(3, 0.25f), FINAL_SALES(4, 0.35f);
 
     int id;
     float discountValue;
